dfe7b775d02a760e09d2ba1291ca7b60c9478ab6
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2023-02-01 13:15:40 +0000

    Finished streamlines module

--- streamlines.py
+++ streamlines.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 '''
 
 
-def streamlines(u, limits= np.array([[-10,-10],[10,10]]), dt= 0.1):
+def streamlines(u, limits= np.array([[-10,-10],[10,10]]), dt= 0.01):
 
     xmin = limits[0][0]
     xmax = limits[1][0]
@@ -14,10 +14,10 @@ def streamlines(u, limits= np.array([[-10,-10],[10,10]]), dt= 0.1):
     ymax = limits[1][1]
 
     # creating a grid of initial points
-    x_values = np.linspace(xmin, xmax, 10)
-    y_values = np.linspace(ymin, ymax, 10)
+    print('Creating Grid')
+    x_values = np.linspace(xmin, xmax, 5)
+    y_values = np.linspace(ymin, ymax, 5)
     grid = np.array([[x_values[i],y_values[j]] for i in range(len(x_values)) for j in range(len(y_values))])
-
 
 
     # for a specific streamline starting at x_init
@@ -28,24 +28,28 @@ def streamlines(u, limits= np.array([[-10,-10],[10,10]]), dt= 0.1):
         x_init = x_init.astype(np.float64)
 
         # forward streamline
-
+        print('Calculating Forward Streamline')
         l_forward = np.array([x_init])
 
         x = x_init
+        iterations = 0
 
-        while xmin <= x[0] and xmax >= x[0] and ymin <= x[1] and ymax >= x[1]:
+        while xmin <= x[0] and xmax >= x[0] and ymin <= x[1] and ymax >= x[1] and iterations < 10000:
             l_forward = np.append(l_forward, np.array([x]), axis= 0)
             x = x + u(x)*dt
+            iterations += 1
 
         # backward streamline
-
+        print('Calculating Backward Streamline')
         l_backward = np.array([x_init])
 
         x = x_init
+        iterations = 0
 
-        while xmin <= x[0] and xmax >= x[0] and ymin <= x[1] and ymax >= x[1]:
+        while xmin <= x[0] and xmax >= x[0] and ymin <= x[1] and ymax >= x[1] and iterations < 10000:
             l_backward = np.append(l_backward, np.array([x]), axis= 0)
             x = x - u(x)*dt
+            iterations += 1
 
 
         # combining forward and backward
@@ -55,7 +59,7 @@ def streamlines(u, limits= np.array([[-10,-10],[10,10]]), dt= 0.1):
         x = [x[0] for x in l]
         y = [x[1] for x in l]
 
-
+        print('Plotting Streamline')
         plt.plot(x, y, color= 'b')
 
 
@@ -71,6 +75,6 @@ a = 1
 b = 1
 
 def u(x):
-    return np.array([(b*x[0]-a*x[1])/((x[0]**2+x[1]**2)**0.5),(a*x[0]+b*x[1])/((x[0]**2+x[1]**2)**0.5)])
+    return np.array([(b*x[0]-a*x[1])*(np.linalg.norm(x)),(a*x[0]+b*x[1])*(np.linalg.norm(x))])
 
 streamlines(u)
